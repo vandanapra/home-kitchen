500e088d32458ee88712d2bf1d489db3be119668
vandanapra <vandanaiec3093@gmail.com>
2025-12-16 21:15:46 +0530

    admin register

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(FoodItem)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Notification)
 
-# Register your models here.
