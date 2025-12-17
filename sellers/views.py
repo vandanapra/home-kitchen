129ddebbb0bdd04a8c45d47a6e237255515bf366
vandanapra <vandanaiec3093@gmail.com>
2025-12-17 20:43:26 +0530

    fix

--- sellers/views.py
+++ sellers/views.py
@@ -4,25 +4,11 @@ from rest_framework.response import Response
 from .models import SellerProfile
 from .serializers import SellerProfileSerializer
 from common.permissions import IsSeller
-
-class SellerProfileView(APIView):
-    permission_classes = [IsSeller]
-
-    def get(self, request):
-        profile = SellerProfile.objects.get(user=request.user)
-        serializer = SellerProfileSerializer(profile)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = SellerProfileSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
-        return Response(serializer.data)
     
 
 class SellerProfileView(APIView):
     permission_classes = [IsAuthenticated]
-    permission_classes = [IsSeller]
+    # permission_classes = [IsSeller]
     
 
     def get(self, request):
