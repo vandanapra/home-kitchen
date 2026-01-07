4092d004f93e09c5b51dcab78cfcde5441acae40
vandanapra <vandanaiec3093@gmail.com>
2026-01-07 12:39:43 +0530

    fix

--- home_kitchen/asgi.py
+++ home_kitchen/asgi.py
@@ -2,13 +2,13 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import notifications.routing
+import orders.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_kitchen.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(notifications.routing.websocket_urlpatterns)
+        URLRouter(orders.routing.websocket_urlpatterns)
     ),
 })
