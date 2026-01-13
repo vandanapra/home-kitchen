017358443050a83679b57e62a99b949fc2a654d6
vandanapra <vandanaiec3093@gmail.com>
2026-01-13 14:33:41 +0530

    fix

--- home_kitchen/settings.py
+++ home_kitchen/settings.py
@@ -235,7 +235,6 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "vandanaiec303@gmail.com"        # 🔴 apna email
-EMAIL_HOST_PASSWORD = "lbbz nqto cono hbzm"      # 🔴 gmail app password
-
-DEFAULT_FROM_EMAIL = "Home Kitchen <yourgmail@gmail.com>"
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
