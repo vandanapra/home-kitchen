addbcc40c0dcc1e71cc65d3adebfeceb75096c1f
vandanapra <112847023+prakashvand@users.noreply.github.com>
2026-03-02 00:35:32 +0530

    fix

--- home_kitchen/settings.py
+++ home_kitchen/settings.py
@@ -20,17 +20,10 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-r*mai=c8r(x^*!(#7+xsdmc+uday@npnnjasb%n^dikrs-4msp'
-# SECRET_KEY = os.getenv("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+
 DEBUG = os.getenv("DEBUG") == "False"
-ALLOWED_HOSTS = ["api-kitchen.vamsmechatronica.in"]
+ALLOWED_HOSTS = ["api-kitchen.vamsmechatronica.in","*"]
 
 
 # Application definition
@@ -56,15 +49,7 @@ INSTALLED_APPS = [
 ]
 AUTH_USER_MODEL = 'users.User'
 
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",   # 👈 TOP
@@ -96,28 +81,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'home_kitchen.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'home_kitchen_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '1@Aachal',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
 
 
